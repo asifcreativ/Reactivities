34ae30653f7d1db02a358785b16451a1991fa9f5
Muhammad Asif <muhammadasif@Muhammads-MBP.attlocal.net>
2020-01-02 11:12:21 -0800

    Section 4: Building a CRUD application in .Net Core using the CQRS + Mediator pattern

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -26,6 +22,7 @@ namespace API
                 {
                     var context = service.GetRequiredService<DataContext>();
                     context.Database.Migrate(); // create database if it does not exist.
+                    Seed.SeedData(context); // create seed data
                 }
                 catch (Exception ex)
                 {
